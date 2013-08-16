9d83138e3867b354451ded3894bbf9ec027d25df
Tien <Hoang>
2013-08-16 08:40:32 +0700

    Temp

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,8 @@
+/**
+ * User: tienhd
+ * Date: 8/16/13
+ * Time: 8:40 AM
+ */
+public class MainApp
+{
+}
---
+++ src/test/java/endtoendtest/EndToEndTest.java
@@ -0,0 +1,10 @@
+package endtoendtest;
+
+/**
+ * User: tienhd
+ * Date: 8/16/13
+ * Time: 8:40 AM
+ */
+public class EndToEndTest
+{
+}
