c622d769a0f978da73efbb8a2690f7b66c5b1e76
Admin <tuananh00473@gmail.com>
2013-08-16 11:07:33 +0700

    testStartGameAndPlaySomeStepThenEndGame()  compile GREEN

--- src/main/java/com/qsoft/kata5/ui/control/MainController.java
+++ src/main/java/com/qsoft/kata5/ui/control/MainController.java
@@ -1,30 +1,83 @@
 package com.qsoft.kata5.ui.control;
 
+import com.qsoft.kata5.ui.view.Cell;
 import com.qsoft.kata5.ui.view.MainWindow;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * User: Admin
  * Date: 8/16/13
  * Time: 9:50 AM
  */
-public class MainController
+public class MainController implements ActionListener
 {
     private MainWindow mainWindow;
-
+    private boolean startXMoveFirst = true;
 
     public MainController()
     {
         mainWindow = new MainWindow(this);
-        mainWindow.show();
+        mainWindow.setVisible(true);
     }
 
     public void doStartGame()
     {
         mainWindow.getLbStatus().setText(MainWindow.STT_STARTED);
+        buildCaroPanel();
+    }
+
+    private void buildCaroPanel()
+    {
+        Cell[][] cells = mainWindow.getCells();
+        for (int i = 0; i < MainWindow.MAX_ROW; i++)
+        {
+            for (int j = 0; j < MainWindow.MAX_COL; j++)
+            {
+                cells[i][j] = new Cell(this);
+                cells[i][j].setName(i + "_" + j);
+                cells[i][j].setFont(new Font("", Font.BOLD, 25));
+                mainWindow.getCaroPanel().add(cells[i][j]);
+            }
+        }
     }
 
     public void doEndGame()
     {
         mainWindow.getLbStatus().setText(MainWindow.STT_ENDED);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        Cell cell = (Cell) e.getSource();
+        if (cell.isCanTick())
+        {
+            if (isStartXMoveFirst())
+            {
+                cell.setText("X");
+                cell.setEnabled(false);
+                setStartXMoveFirst(false);
+            }
+            else
+            {
+                cell.setText("O");
+                cell.setEnabled(false);
+                setStartXMoveFirst(true);
+            }
+            cell.setCanTick(false);
+        }
+    }
+
+    public boolean isStartXMoveFirst()
+    {
+        return startXMoveFirst;
+    }
+
+    public void setStartXMoveFirst(boolean startXMoveFirst)
+    {
+        this.startXMoveFirst = startXMoveFirst;
     }
 }
---
+++ src/main/java/com/qsoft/kata5/ui/view/Cell.java
@@ -0,0 +1,31 @@
+package com.qsoft.kata5.ui.view;
+
+import com.qsoft.kata5.ui.control.MainController;
+
+import javax.swing.*;
+
+/**
+ * User: Admin
+ * Date: 8/16/13
+ * Time: 10:15 AM
+ */
+public class Cell extends JButton
+{
+    private boolean canTick = true;
+
+    public Cell(MainController mainController)
+    {
+        super();
+        addActionListener(mainController);
+    }
+
+    public boolean isCanTick()
+    {
+        return canTick;
+    }
+
+    public void setCanTick(boolean canTick)
+    {
+        this.canTick = canTick;
+    }
+}
