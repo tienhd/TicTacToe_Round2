5120e1bed64cb0da8abb5257a9cb73af684ea28e
Admin <tuananh00473@gmail.com>
2013-08-16 08:54:33 +0700

    call TicTacToeDriver class

---
+++ src/test/java/endtoendtest/ApplicationRunner.java
@@ -0,0 +1,16 @@
+package endtoendtest;
+
+/**
+ * User: Admin
+ * Date: 8/16/13
+ * Time: 8:50 AM
+ */
+public class ApplicationRunner
+{
+    TicTacToeDriver ticTacToeDriver = new TicTacToeDriver();
+
+    public void startGame()
+    {
+
+    }
+}
