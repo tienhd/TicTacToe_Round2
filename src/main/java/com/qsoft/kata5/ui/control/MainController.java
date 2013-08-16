44ab7dda0d0f45ffe7a2e01b228bfdcbe329f1a8
Admin <tuananh00473@gmail.com>
2013-08-16 11:44:51 +0700

    testStartGameThenPlayUtilXWin() compile  GREEN

--- src/main/java/com/qsoft/kata5/ui/control/MainController.java
+++ src/main/java/com/qsoft/kata5/ui/control/MainController.java
@@ -70,12 +70,55 @@ public class MainController implements ActionListener
                 setStartXMoveFirst(true);
             }
             cell.setCanTick(false);
+            checkWin();
             numberCellTicked++;
             if (numberCellTicked == 9)
             {
                 mainWindow.getLbStatus().setText(MainWindow.STT_DRAWED);
             }
         }
+    }
+
+    private void checkWin()
+    {
+        String winnerSymbol = getWinner(mainWindow.getCells());
+        if (winnerSymbol != null && !winnerSymbol.equals(""))
+        {
+            mainWindow.getLbStatus().setText(winnerSymbol + " WON !");
+        }
+    }
+
+    public static String getWinner(Cell[][] cells)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (cells[i][0].getText().equals(cells[i][1].getText()) &&
+                    cells[i][0].getText().equals(cells[i][2].getText()))
+            {
+                return cells[i][0].getText();
+            }
+        }
+        for (int j = 0; j < 3; j++)
+        {
+            if (cells[0][j].getText().equals(cells[1][j].getText()) &&
+                    cells[0][j].getText().equals(cells[2][j].getText()))
+            {
+                return cells[0][j].getText();
+            }
+        }
+        //
+        if (cells[0][0].getText().equals(cells[1][1].getText()) &&
+                cells[0][0].getText().equals(cells[2][2].getText()))
+        {
+            return cells[0][0].getText();
+        }
+        //
+        if (cells[2][0].getText().equals(cells[1][1].getText()) &&
+                cells[2][0].getText().equals(cells[0][2].getText()))
+        {
+            return cells[2][0].getText();
+        }
+        return null;
     }
 
     public boolean isStartXMoveFirst()
