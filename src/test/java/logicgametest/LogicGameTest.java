d47c337d4761f2222b7f37c51f63fd4a53fed326
Admin <tuananh00473@gmail.com>
2013-08-19 08:54:25 +0700

    testLogicGameForXWinInRightCross() compile RED

--- src/test/java/logicgametest/LogicGameTest.java
+++ src/test/java/logicgametest/LogicGameTest.java
@@ -49,4 +49,15 @@ public class LogicGameTest
         cells[2][0].setText("O");
         assertEquals("O", LogicGame.getWinner(cells));
     }
+
+    @Test
+    public void testLogicGameForXWinInRightCross()
+    {
+        cells[0][0].setText("X");
+        cells[1][0].setText("O");
+        cells[1][1].setText("X");
+        cells[2][1].setText("O");
+        cells[2][2].setText("X");
+        assertEquals("X", LogicGame.getWinner(cells));
+    }
 }
