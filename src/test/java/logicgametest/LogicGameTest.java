5d9080d8512be3f339a66ce5717346c193cd3e33
Admin <tuananh00473@gmail.com>
2013-08-19 08:42:37 +0700

    testLogicGameForOWinInColumn() compile RED

--- src/test/java/logicgametest/LogicGameTest.java
+++ src/test/java/logicgametest/LogicGameTest.java
@@ -31,11 +31,22 @@ public class LogicGameTest
     @Test
     public void testLogicGameForOWinInRow()
     {
-        cells[0][0].setText("X");
+        cells[0][0].setText("O");
+        cells[1][0].setText("X");
+        cells[0][1].setText("O");
+        cells[1][1].setText("X");
+        cells[0][2].setText("O");
+        assertEquals("O", LogicGame.getWinner(cells));
+    }
+
+    @Test
+    public void testLogicGameForOWinInColumn()
+    {
+        cells[0][0].setText("O");
+        cells[2][2].setText("X");
         cells[1][0].setText("O");
-        cells[0][1].setText("X");
-        cells[1][1].setText("O");
-        cells[0][2].setText("X");
-        assertEquals("X", LogicGame.getWinner(cells));
+        cells[1][1].setText("X");
+        cells[2][0].setText("O");
+        assertEquals("O", LogicGame.getWinner(cells));
     }
 }
